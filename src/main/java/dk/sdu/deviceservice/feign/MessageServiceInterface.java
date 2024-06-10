afb9ee587cee433b9ce92e4c709e6f126a80fc6a
Rasmus Helleberg Eriksen <raeri21@student.sdu.dk>
2024-06-10 14:47:04 +0200

    Updated message service endpoint

--- src/main/java/dk/sdu/deviceservice/feign/MessageServiceInterface.java
+++ src/main/java/dk/sdu/deviceservice/feign/MessageServiceInterface.java
@@ -2,15 +2,15 @@ package dk.sdu.deviceservice.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.UUID;
 
 @FeignClient("MESSAGE-SERVICE")
 public interface MessageServiceInterface {
-    @PostMapping("api/v1/mqtt/device/identify")
+    @GetMapping("api/v1/mqtt/device/identify")
     @ResponseStatus(HttpStatus.OK)
     void indetifyDevice(@PathVariable UUID uuid);
 }
